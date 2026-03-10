3bb2cb824f986d61e6f6f6d21db4bc27b6c96114
Luan Thális da Silva <luan.thalis@hotmail.com>
2026-03-10 20:27:07 -0300

    gsgiosg

---
+++ src/Core/Core.Domain/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Domain;
+
+public interface IAggregateRoot
+{
+    Guid Id { get; }
+    int Version { get; }
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    void ClearEvents();
+    void LoadFromHistory(IEnumerable<IDomainEvent> events);
+}
+asfbadgikhnadiogabdoi
